f2c240887c21a85d01f56df921bf94a83ea1b18f
ben615 <ben.matteson89@gmail.com>
2025-12-11 11:07:07 -0600

    Add local storage for saved spells and rename counter to mytome

--- TomeKeeper/Services/SavedSpellsService.cs
+++ TomeKeeper/Services/SavedSpellsService.cs
@@ -1,16 +1,34 @@
-﻿using TomeKeeper.Models;
+﻿using Microsoft.JSInterop;
+using System.Text.Json;
+using TomeKeeper.Models;
+
 namespace TomeKeeper.Services
 {
     public class SavedSpellsService
     {
+        private readonly IJSRuntime _jsRuntime;
+
         private ISpellListItemsCacheService _spellListItemsCacheService { get; set; }
 
-        public SavedSpellsService(ISpellListItemsCacheService spellListItemsCacheService)
+        public SavedSpellsService(ISpellListItemsCacheService spellListItemsCacheService, IJSRuntime jsRuntime)
         {
             _spellListItemsCacheService = spellListItemsCacheService;
+            _jsRuntime = jsRuntime;
+        }
+        public async Task InitializeSavedSpellsAsync()
+        {
+            var jsonString = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "UserSpellList");
+            if (!string.IsNullOrEmpty(jsonString))
+            {
+                var items = JsonSerializer.Deserialize<List<SpellListItem>>(jsonString);
+                if (items != null)
+                {
+                    SavedSpellListItems = items;
+                }
+            }
         }
 
-        public void AddSpell(string spellIndex)
+        public async Task AddSpell(string spellIndex)
         {
             // Prevent duplicates and invalid entries
             if (string.IsNullOrWhiteSpace(spellIndex) ||
@@ -26,9 +44,14 @@ namespace TomeKeeper.Services
                 //TODO: Get the spell from the API or throw an error
                 return;
             }
+
             SavedSpellListItems.Add(cachedSpellListItem);
+
+            var jsonString = JsonSerializer.Serialize(SavedSpellListItems);
+            await _jsRuntime.InvokeVoidAsync("localStorage.setItem", "UserSpellList", jsonString);
         }
 
+        //TODO: make not static since static instances are shared among all user sessions
         public static List<SpellListItem> SavedSpellListItems { get; set; } = new List<SpellListItem>();
     }
 }
---
+++ TomeKeeper/Services/StorageService.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using TomeKeeper.Models;
+
+namespace TomeKeeper.Services
+{
+    public class StorageService
+    {
+        public string ConvertSpellListToJson(List<SpellListItem> spellList)
+        {
+            var jsonString = JsonSerializer.Serialize(spellList);
+            return jsonString;
+        }
+    }
+}
