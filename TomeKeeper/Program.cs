4148a04093409551a0a7e84457ed5ca0b8ebd68e
ben615 <ben.matteson89@gmail.com>
2025-12-13 16:10:34 -0600

    refactor scroll button to be its own custom control

--- TomeKeeper/Program.cs
+++ TomeKeeper/Program.cs
@@ -10,7 +10,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<DragDropService>();
-builder.Services.AddScoped<IExpandableScrollButtonViewModel, ExpandableScrollButtonViewModel>();
+
+builder.Services.AddTransient<IExpandableScrollButtonViewModel, ExpandableScrollButtonViewModel>();
 
 builder.Services.AddSingleton<IAPIService, APIService>();
 builder.Services.AddSingleton<ITextFormatterService, TextFormatterService>();
